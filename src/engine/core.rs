f375c68cea6bff240d715f1cdc9d8871f5ff6769
F1L1P <mlodzik.filip@wp.pl>
2023-06-20 19:14:25 +0200

    one of the biggest refactors

--- src/engine/core.rs
+++ src/engine/core.rs
@@ -4,7 +4,10 @@ use super::object::Object;
 // use super::matrices::{ model_matrix };
 // use super::prefab::{get_prefabs, PrefabList};
 use terminal_size::terminal_size;
+use device_query::{ DeviceState };
 use glium::{ glutin};
+use glium::Surface;
+use super::game_loop::game_loop;
 use super::scene::Scene;
 
 //create init type
@@ -153,4 +156,175 @@ pub fn init() -> InitType
 
     (terminal_res, terminal_fb, event_loop, display, program,ui_program, params,ui_params, game)
 
+}
+
+type run_event_loop_type = (
+    (u32, u32),
+    TerminalFrameBuffer,
+    glutin::event_loop::EventLoop<()>,
+    glium::Display,
+    glium::Program,
+    glium::Program,
+    glium::DrawParameters<'static>,
+    glium::DrawParameters<'static>,
+    Game,
+);
+
+pub fn run_event_loop(
+    terminal_res: (u32, u32),
+    terminal_fb: TerminalFrameBuffer,
+    event_loop: glutin::event_loop::EventLoop<()>,
+    display: glium::Display,
+    program: glium::Program,
+    ui_program: glium::Program,
+    params: glium::DrawParameters<'static>,
+    ui_params: glium::DrawParameters<'static>,
+    mut game: Game,
+){
+
+    let mut terminal_res = terminal_res;
+    let mut terminal_fb = terminal_fb;
+
+    let light = [1.4, 0.4, -0.7f32];
+    let device_state = DeviceState::new();
+    let mut accumulator = std::time::Duration::new(0, 0);
+    let fixed_timestep = std::time::Duration::from_nanos(16_666_667);
+    let mut next_frame_time = std::time::Instant::now();
+
+    event_loop.run(move |event, _, control_flow| {
+        let texture = glium::texture::Texture2d
+            ::empty_with_format(
+                &display,
+                glium::texture::UncompressedFloatFormat::U8U8U8U8,
+                glium::texture::MipmapsOption::NoMipmap,
+                terminal_res.0,
+                terminal_res.1
+            )
+            .unwrap();
+
+        // Create a depth buffer for off-screen rendering
+        let depthbuffer = glium::framebuffer::DepthRenderBuffer
+            ::new(&display, glium::texture::DepthFormat::F32, terminal_res.0, terminal_res.1)
+            .unwrap();
+
+        // Create a framebuffer for off-screen rendering
+        let mut framebuffer: glium::framebuffer::SimpleFrameBuffer = glium::framebuffer::SimpleFrameBuffer
+            ::with_depth_buffer(&display, &texture, &depthbuffer)
+            .unwrap();
+
+        // Check res and update if changed
+        let new_terminal_res = terminal_size().unwrap();
+        let new_terminal_res: (u32, u32) = (
+            u32::from(new_terminal_res.0.0),
+            u32::from(new_terminal_res.1.0),
+        );
+        if new_terminal_res != terminal_res {
+            terminal_res = new_terminal_res;
+            terminal_fb = TerminalFrameBuffer::new(
+                (terminal_res.0 as usize) / 2,
+                terminal_res.1 as usize,
+                Color { r: 0, g: 0, b: 0 }
+            );
+            TerminalFrameBuffer::update_res(
+                &mut terminal_fb,
+                new_terminal_res.0 as usize,
+                new_terminal_res.1 as usize
+            );
+        }
+
+        *control_flow = glutin::event_loop::ControlFlow::Poll;
+
+        match event {
+            glutin::event::Event::WindowEvent {
+                event: glutin::event::WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = glutin::event_loop::ControlFlow::Exit;
+                return;
+            }
+
+            glutin::event::Event::MainEventsCleared => {
+                let now = std::time::Instant::now();
+                accumulator += now - next_frame_time;
+                next_frame_time = now;
+
+                while accumulator >= fixed_timestep {
+                    //--------------------------------- Sort of a game loop ---------------------------------
+
+                    game_loop(&device_state, terminal_res, &mut game);
+
+                    accumulator -= fixed_timestep;
+                }
+
+                //--------------------------------- Render (post update) ---------------------------------
+
+                framebuffer.clear_color_and_depth(
+                    (105.0 / 255.0, 109.0 / 255.0, 219.0 / 255.0, 1.0),
+                    1.0
+                );
+
+                for object in &game.get_scene().objects {
+                    let uniforms =
+                        uniform! {
+                        model: object.model,
+                        view: game.camera.view_matrix(),
+                        perspective: game.camera.perspective_matrix(),
+                        u_light: light,
+                        tex: &object.texture,
+                    };
+
+                    framebuffer.draw(&object.vb, &object.ib, &program, &uniforms, &params).unwrap();
+                }
+
+                //--------------------------------- UI ---------------------------------
+
+                for ui_elem in &game.get_ui_elems().elems {
+
+                    // let behavior = glium::uniforms::SamplerBehavior {
+                    //     minify_filter:  glium::uniforms::MinifySamplerFilter::Nearest,
+                    //     magnify_filter: glium::uniforms::MagnifySamplerFilter::Nearest,
+                    //     ..Default::default()
+                    // };
+
+                    let uniforms =
+                        uniform! {
+                        // tex: glium::uniforms::Sampler(&ui_elem.texture, behavior),
+                        tex: &ui_elem.texture,
+                    };
+
+                    framebuffer
+                        .draw(&ui_elem.vb, &ui_elem.ib, &ui_program, &uniforms, &ui_params)
+                        .unwrap();
+                }
+            }
+
+            _ => {
+                return;
+            }
+        }
+
+        //get pixels from display
+        //TODO: use framebuffer.read_to_pixel_buffer
+        let pixels: glium::texture::RawImage2d<u8> = texture.read();
+        terminal_fb.clear();
+        for i in 0..pixels.data.len() / 4 {
+            let r = pixels.data[i * 4];
+            let g = pixels.data[i * 4 + 1];
+            let b = pixels.data[i * 4 + 2];
+
+            let x =
+                ((i % (pixels.width as usize)) * (terminal_res.0 as usize)) /
+                    (pixels.width as usize) -
+                (terminal_res.0 as usize) / 4;
+            let y =
+                (terminal_res.1 as usize) -
+                ((i / (pixels.width as usize)) * (terminal_res.1 as usize)) /
+                    (pixels.height as usize);
+
+            let color = Color { r, g, b };
+
+            terminal_fb.set_pixel(x, y, color);
+        }
+        terminal_fb.draw_frame();
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
 #[macro_use]
 extern crate glium;
-use glium::{ glutin, Surface };
 
-//-------------- Terminal Stuff ------------------------
-use device_query::{ DeviceState };
-use terminal_size::terminal_size;
 // -----------------------------------------------------
 use fontdue::Font;
 //------------------ My stuff --------------------------
 mod engine;
 use engine::scene::Scene;
-use engine::ascii_render::{ Color, TerminalFrameBuffer };
-use engine::core::{ init};
+use engine::core::{ init, run_event_loop};
 use engine::matrices::model_matrix;
-use engine::game_loop::game_loop;
 use engine::object::Object;
-// use engine::ui::draw_rect;
 use engine::ui::{draw_text, draw_rect};
 // -----------------------------------------------------
 
 fn main() {
     let (
-        mut terminal_res,
-        mut terminal_fb,
+        terminal_res,
+        terminal_fb,
         event_loop,
         display,
         program,
@@ -98,151 +91,7 @@ fn main() {
 
     game.add_ui_elem(draw_rect(0.0, 0.75, 0.25, 0.25, "assets/sprites/align.png", &display));
 
-    // println!("{:?}", game.get_ui_elems());
-
-    // return;
-
-    let light = [1.4, 0.4, -0.7f32];
-    let device_state = DeviceState::new();
-    let mut accumulator = std::time::Duration::new(0, 0);
-    let fixed_timestep = std::time::Duration::from_nanos(16_666_667);
-    let mut next_frame_time = std::time::Instant::now();
-
     // Main loop
-    event_loop.run(move |event, _, control_flow| {
-        let texture = glium::texture::Texture2d
-            ::empty_with_format(
-                &display,
-                glium::texture::UncompressedFloatFormat::U8U8U8U8,
-                glium::texture::MipmapsOption::NoMipmap,
-                terminal_res.0,
-                terminal_res.1
-            )
-            .unwrap();
-
-        // Create a depth buffer for off-screen rendering
-        let depthbuffer = glium::framebuffer::DepthRenderBuffer
-            ::new(&display, glium::texture::DepthFormat::F32, terminal_res.0, terminal_res.1)
-            .unwrap();
-
-        // Create a framebuffer for off-screen rendering
-        let mut framebuffer: glium::framebuffer::SimpleFrameBuffer = glium::framebuffer::SimpleFrameBuffer
-            ::with_depth_buffer(&display, &texture, &depthbuffer)
-            .unwrap();
-
-        // Check res and update if changed
-        let new_terminal_res = terminal_size().unwrap();
-        let new_terminal_res: (u32, u32) = (
-            u32::from(new_terminal_res.0.0),
-            u32::from(new_terminal_res.1.0),
-        );
-        if new_terminal_res != terminal_res {
-            terminal_res = new_terminal_res;
-            terminal_fb = TerminalFrameBuffer::new(
-                (terminal_res.0 as usize) / 2,
-                terminal_res.1 as usize,
-                Color { r: 0, g: 0, b: 0 }
-            );
-            TerminalFrameBuffer::update_res(
-                &mut terminal_fb,
-                new_terminal_res.0 as usize,
-                new_terminal_res.1 as usize
-            );
-        }
-
-        *control_flow = glutin::event_loop::ControlFlow::Poll;
-
-        match event {
-            glutin::event::Event::WindowEvent {
-                event: glutin::event::WindowEvent::CloseRequested,
-                ..
-            } => {
-                *control_flow = glutin::event_loop::ControlFlow::Exit;
-                return;
-            }
-
-            glutin::event::Event::MainEventsCleared => {
-                let now = std::time::Instant::now();
-                accumulator += now - next_frame_time;
-                next_frame_time = now;
-
-                while accumulator >= fixed_timestep {
-                    //--------------------------------- Sort of a game loop ---------------------------------
-
-                    game_loop(&device_state, terminal_res, &mut game);
-
-                    accumulator -= fixed_timestep;
-                }
-
-                //--------------------------------- Render (post update) ---------------------------------
-
-                framebuffer.clear_color_and_depth(
-                    (105.0 / 255.0, 109.0 / 255.0, 219.0 / 255.0, 1.0),
-                    1.0
-                );
-
-                for object in &game.get_scene().objects {
-                    let uniforms =
-                        uniform! {
-                        model: object.model,
-                        view: game.camera.view_matrix(),
-                        perspective: game.camera.perspective_matrix(),
-                        u_light: light,
-                        tex: &object.texture,
-                    };
-
-                    framebuffer.draw(&object.vb, &object.ib, &program, &uniforms, &params).unwrap();
-                }
-
-                //--------------------------------- UI ---------------------------------
-
-                for ui_elem in &game.get_ui_elems().elems {
-
-                    // let behavior = glium::uniforms::SamplerBehavior {
-                    //     minify_filter:  glium::uniforms::MinifySamplerFilter::Nearest,
-                    //     magnify_filter: glium::uniforms::MagnifySamplerFilter::Nearest,
-                    //     ..Default::default()
-                    // };
-
-                    let uniforms =
-                        uniform! {
-                        // tex: glium::uniforms::Sampler(&ui_elem.texture, behavior),
-                        tex: &ui_elem.texture,
-                    };
-
-                    framebuffer
-                        .draw(&ui_elem.vb, &ui_elem.ib, &ui_program, &uniforms, &ui_params)
-                        .unwrap();
-                }
-            }
-
-            _ => {
-                return;
-            }
-        }
-
-        //get pixels from display
-        //TODO: use framebuffer.read_to_pixel_buffer
-        let pixels: glium::texture::RawImage2d<u8> = texture.read();
-        terminal_fb.clear();
-        for i in 0..pixels.data.len() / 4 {
-            let r = pixels.data[i * 4];
-            let g = pixels.data[i * 4 + 1];
-            let b = pixels.data[i * 4 + 2];
-
-            let x =
-                ((i % (pixels.width as usize)) * (terminal_res.0 as usize)) /
-                    (pixels.width as usize) -
-                (terminal_res.0 as usize) / 4;
-            let y =
-                (terminal_res.1 as usize) -
-                ((i / (pixels.width as usize)) * (terminal_res.1 as usize)) /
-                    (pixels.height as usize);
-
-            let color = Color { r, g, b };
-
-            terminal_fb.set_pixel(x, y, color);
-        }
-        terminal_fb.draw_frame();
-    });
+    run_event_loop(terminal_res, terminal_fb, event_loop, display, program, ui_program, params, ui_params, game);
+    
 }
